b7030a7ac54e6a6b28f3eace79b0a15fb46c5c7c
Luis Tupa <luis.tupa@neoauto.pe>
2023-05-26 22:52:34 -0500

    feat: add useFullScreenHeight test and covered image test

--- src/components/image/image.spec.tsx
+++ src/components/image/image.spec.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import { mockAllIsIntersecting } from 'react-intersection-observer/test-utils'
 import Image from './image'
 
@@ -11,6 +11,7 @@ describe('Image component', () => {
     expect(imageElement).toBeInTheDocument()
     expect(imageElement.src).toContain('data:image/png;base64')
   })
+
   test('Image => render with provided source', () => {
     render(<Image src="test.jpg" alt="Test Image" />)
 
@@ -20,5 +21,27 @@ describe('Image component', () => {
 
     expect(imageElement.src).toContain('test.jpg')
     expect(imageElement.srcset).toBe('')
+  })
+
+  test('Image => render with provided source set', () => {
+    render(<Image src="test.jpg" srcSet="test-large.jpg 2x, test-small.jpg 1x" alt="Test Image" />)
+
+    const imageElement: HTMLImageElement = screen.getByAltText('Test Image')
+
+    mockAllIsIntersecting(true)
+
+    expect(imageElement.srcset).toContain('test-large.jpg 2x, test-small.jpg 1x')
+  })
+
+  test('Image => render with fallback source on loading error', async () => {
+    render(<Image src="test.jpg" alt="Test Image" />)
+
+    const imageElement: HTMLImageElement = screen.getByAltText('Test Image')
+
+    mockAllIsIntersecting(true)
+
+    fireEvent.error(imageElement)
+
+    expect(imageElement.src).toContain('data:image/png;base64')
   })
 })
